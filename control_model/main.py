43331ce3c7e5f510b4ba35b65dfe6ea9025ac9e9
Antony Chan <antony.sigma@gmail.com>
2023-05-16 16:07:46 -0700

    Replace two complex zeros with two real poles
    Intead of pulling the root locus of the 250Hz poles to 100Hz with two
    zeros on the j*omega axis, we add two poles on the negative real axis.
    This forces the 250Hz root locus to the left half plane.
    
    The new poles has a branch cut, passing the desired domainating pole
    position (0.68 damping, ~100Hz oscillation freq). It is conditionally
    stable with gain < 1e9, so please use it with caution.

--- control_model/main.py
+++ control_model/main.py
@@ -1,33 +1,46 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt   # MATLAB plotting functions
-from control import (feedback, tf, series, step_response, root_locus, zpk, step_info, rootlocus_pid_designer)
+from control import (feedback, tf, series, step_response, root_locus, zpk, step_info, rootlocus_pid_designer, pzmap)
 
 post_filter = tf([952206], [1, 1916.6, 952206])
 
 natural_freq = 250.0 * 2 * np.pi
-piezo_actuator = zpk([], [-natural_freq * 1j, natural_freq * 1j], gain=1.0)
-controller = zpk([-800, -550, -500, -400], [0.0], gain=1.0)
+#damping = 0.01
+piezo_actuator = zpk([], [-10 + natural_freq*1j, -10 - natural_freq * 1j], gain=1.0)
+print(f'Plant =\n{piezo_actuator}')
+
+pole_branch = -450
+
+controller = zpk([-800, -200], [0.0, pole_branch-50, pole_branch+50], gain=1.0)
+print(f'Controller=\n{controller}')
 
 # Compute the open-loop transfer function
+#sys = series(controller, post_filter, piezo_actuator).sample(5e-3, 'bilinear')
 sys = series(controller, post_filter, piezo_actuator)
+print(f'Open loop transfer function=\n{sys}')
+
 
 # Root lcous plot for the system
 plt.figure(1)
-root_locus(sys, xlim=[-1200, 200])
+root_locus(sys, xlim=[-1200, 20], ylim=[-2000, 2000])
 plt.title('Root locus plot')
 plt.show(block=False)
 
 # Compute the close-loop transfer function
-sys2 = feedback(sys * 8e-3)
-#sys2 = rootlocus_pid_designer(sys, Kp0=1.0)
+gain = 6e8
+sys2 = feedback(sys * gain)
+print(f'Close loop transfer function=\n{sys2}')
+
+discretized_controller = (controller * gain).sample(5e-3, 'bilinear')
+print(f'Discretized controller=\n{discretized_controller}')
 
 # Step response for the system
 plt.figure(2)
 time, yout = step_response(sys2, T=100e-3)
 
 info = step_info(sys2)
-print(info)
+#print(info)
 overshoot = info['Peak'] - 1.0
 
 plt.plot(time * 1e3, yout)
